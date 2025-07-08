ed6eef57bf6364ad7eef676f28259594f90ac0d0
Clifford Halasan <cliffordhalasan11@gmail.com>
2025-07-08 13:16:18 -0800

    added auth functionality

---
+++ lib/actions/users.actions.ts
@@ -0,0 +1,71 @@
+"use server";
+
+import { createAdminClient } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { Query, ID } from "node-appwrite";
+import { parseStringify } from "@/lib/utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])],
+  );
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to create email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    const accountId = await sendEmailOTP({ email });
+    if (!accountId)
+      throw new Error("Failed to create account - no accountId returned");
+
+    if (!existingUser) {
+      const { databases } = await createAdminClient();
+
+      await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        ID.unique(),
+        {
+          email,
+          fullName,
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s",
+          accountId,
+        },
+      );
+    }
+
+    return parseStringify({ accountId });
+  } catch (error) {
+    console.error("Error in createAccount:", error); // Add this line
+    throw error; // Re-throw the error to be caught by the form
+  }
+};
